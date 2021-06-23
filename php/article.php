0ddf2d71e46bd08a3fb84bf99f63c9398f3529fb
olivier <olivier@oclock.io>
2021-06-23 10:15:16 +0200

    templatisation de la page article

--- php/article.php
+++ php/article.php
@@ -1,31 +1,44 @@
 <?php
 require 'inc/header.php';
+
+// Je déclare un tableau associatif contenant toutes les données de mon
+// article
+$article = [
+    'title'    => 'Lorem ipsum dolor article 1',
+    'author'   => 'Darren Collison',
+    'text'     => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sapiente possimus dolor molestias laboriosam et aliquam, blanditiis amet provident corrupti at doloremque quam distinctio. Fugiat quasi sed id! Earum corporis eum laboriosam possimus!',
+    'category' => 'team',
+    'date'     => 'le 10 février 2018',
+    'icon'     => 'icon-dar.png'
+];
+
 ?>
 <h2 class="right__title">
-    Titre de l'article
+    <?php echo $article['title']; ?>
 </h2>
+
 <div class="posts">
     <div class="post post--solo">
 
         <a href="" class="post__category post__category--color-team">
-            team
+            <?php echo $article['category']; ?>
         </a>
         
         <div class="post__meta">
             <img class="post__author-icon" 
-                src="../images/icon-dar.png" alt="">
+                src="../images/<?php echo $article['icon']; ?>" alt="">
 
             <strong class="post__author">
-                Darren Collison
+                <?php echo $article['author']; ?>
             </strong>
 
             <time datetime="2018-02-10">
-                le 10 février 2018
+                <?php echo $article['date']; ?>
             </time>
         </div>
         
         <p class="single">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quis provident culpa accusantium saepe asperiores voluptatem ad nesciunt quas. Pariatur illo libero, velit ea nihil minima doloremque voluptate, aspernatur maiores neque et, cum dolorem harum blanditiis quibusdam dolores quam eum odit fugit culpa ratione dolor atque? Iusto, enim odit commodi corrupti ducimus vitae optio. Quas inventore quasi ex cum velit! Et itaque architecto tenetur debitis libero autem. Recusandae quos, modi repellendus delectus inventore consectetur? Quidem sit at perspiciatis deleniti soluta iste incidunt suscipit. Adipisci neque provident voluptas omnis quia? Id ut repellat cum error adipisci debitis blanditiis consectetur officia molestias quibusdam. Vero a at tenetur? Ipsum sit illo, illum ducimus modi praesentium, nulla maiores nisi repellat asperiores unde quod amet assumenda aspernatur itaque architecto tempora doloribus. Odit eius, laborum praesentium voluptate voluptatibus cupiditate minima! Et at earum debitis cumque dignissimos deleniti accusantium aliquid amet consequatur voluptatibus, nisi repellat quaerat excepturi quod impedit quis sit fugiat accusamus similique ipsa inventore odit commodi! Enim nam praesentium aut animi quidem molestiae iure architecto eos tempore debitis fuga laboriosam cumque ducimus, cupiditate in recusandae sunt quaerat atque, culpa ipsam maxime! Distinctio, ex in pariatur ipsa maxime accusantium quos facilis ullam neque maiores quod sint repudiandae possimus nulla quia consequuntur, voluptates corrupti. Earum adipisci architecto corporis quibusdam eum, corrupti consequuntur aliquid tempora laborum at ullam ratione placeat et illum natus veniam ut suscipit. Minima assumenda incidunt sequi impedit dignissimos qui iusto sapiente est laboriosam pariatur, eaque, cum molestiae perferendis dolorum error quibusdam dolores laudantium odit atque beatae tempore quod natus commodi. Ipsa ipsum molestias iusto, dicta, quibusdam eligendi tenetur eius, ut hic nobis ab sunt fugit maxime quidem optio adipisci odit natus praesentium nisi tempora maiores inventore numquam quas. Magni eligendi molestiae perferendis officiis alias, aliquam tenetur porro commodi animi ea eius at cumque, vitae voluptas nemo corrupti labore mollitia officia aliquid tempore doloremque dignissimos. Quaerat perferendis accusamus molestiae placeat animi deleniti, sint ex odio possimus fugit et aliquid hic voluptates sit reprehenderit similique ab iure, eaque optio quo fugiat facere aspernatur autem. Ipsum cupiditate quas ipsa eligendi quo iusto magnam, fugit, voluptates quam sed distinctio rerum consequatur est aliquam. Magnam expedita recusandae blanditiis obcaecati voluptatibus numquam maxime iste neque quos! Dolores quibusdam debitis a, explicabo assumenda blanditiis illum distinctio repudiandae iure voluptatem sed nesciunt, architecto ut magnam cum? Perspiciatis, reiciendis ducimus enim maiores ut vitae dolore sunt. Eos quam veritatis architecto maiores quasi adipisci!
+            <?php echo $article['text']; ?>
         </p>
 
         <a href="./index.php" class="post__link">back to home</a>
